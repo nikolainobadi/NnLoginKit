a9bf7e850b75e0822e7b0db8e3ff10fd0e3d8317
Nikolai Nobadi <nikolai.rivas@gmail.com>
2022-02-09 17:37:41 -0800

    Initial Commit

---
+++ Tests/NnLoginKitTests/NnLoginKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import NnLoginKit
+
+final class NnLoginKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(NnLoginKit().text, "Hello, World!")
+    }
+}
