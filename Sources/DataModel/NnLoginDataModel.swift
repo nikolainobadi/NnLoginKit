ad9fd1875b2479ca49c5e97f22bf0e6865be8bfb
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-01-03 16:03:41 -0800

    integrated NnLoginActions into NnLoginDataModel

--- Sources/DataModel/NnLoginDataModel.swift
+++ Sources/DataModel/NnLoginDataModel.swift
@@ -26,24 +26,63 @@ public final class NnLoginDataModel: ObservableObject {
     }
 }
 
+
+// MARK: - View Model
 public extension NnLoginDataModel {
     var title: String { isLogin ? "Login" : "Sign Up" }
     var accountButtonText: String { "\(isLogin ? "Don't" : "Already") have an account?" }
+    var viewColors: NnLoginViewConfig.ColorOptions { config.colors }
     var canShowGuestLogin: Bool { options.enableGuestLogin }
     var canShowResetPassword: Bool { options.enableResetPassword }
     var canShowAccountButton: Bool { options.enableToggleLoginType }
     
-    func login() {
+    func login(shouldSkip: Bool = false) {
+        isLoading = true
+        loginFieldError = nil
         
-    }
-    
-    func guestLogin() {
-        
+        Task {
+            do {
+                try await loginOrSignUp(shouldSkip: shouldSkip)
+                await stopLoading()
+            } catch {
+                await stopLoading()
+                await showError(error)
+            }
+        }
     }
 }
 
+
+// MARK: - Private Helpers
 private extension NnLoginDataModel {
     var options: NnLoginViewConfig.LoginOptions { config.options }
+    
+    func loginOrSignUp(shouldSkip: Bool) async throws {
+        if shouldSkip {
+            try await actions.guestLogin()
+        } else {
+            try LoginInfoValidator.validateInfo(email: email, password: password, confirm: isLogin ? nil : confirm)
+            
+            if isLogin {
+                try await actions.login(email: email, password: password)
+            } else {
+                try await actions.signUp(email: email, password: password)
+            }
+        }
+    }
+}
+
+
+// MARK: - MainActor
+private extension NnLoginDataModel {
+    @MainActor func stopLoading() { self.isLoading = false }
+    @MainActor func showError(_ error: Error?) {
+        if let loginFieldError = error as? NnLoginFieldError {
+            self.loginFieldError = loginFieldError
+        } else {
+            self.error = error
+        }
+    }
 }
 
 
@@ -110,3 +149,26 @@ public extension NnLoginViewConfig {
     }
 }
 
+enum LoginInfoValidator {
+    static func validateInfo(email: String, password: String, confirm: String? = nil) throws {
+        guard emailIsValid(email) else { throw NnLoginFieldError.email }
+        guard passwordIsValid(password) else { throw NnLoginFieldError.password }
+        
+        if let confirm = confirm {
+            guard passwordsMatch(password: password, confirm: confirm) else { throw NnLoginFieldError.confirm }
+        }
+    }
+}
+
+
+private extension LoginInfoValidator {
+    static func emailIsValid(_ email: String) -> Bool {
+        let regex = #"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,64}"#
+
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
+        
+        return predicate.evaluate(with: email)
+    }
+    static func passwordIsValid(_ password: String) -> Bool { password != "" && password.count > 5 }
+    static func passwordsMatch(password: String, confirm: String) -> Bool { password == confirm }
+}
