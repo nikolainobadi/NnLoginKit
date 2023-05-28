394412360a39303af4ff37dd0db6f66563a7f290
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-05-27 20:06:56 -0700

    WIP -> added login error message text to EmailLoginView

--- Sources/UI/Views/EmailLoginView.swift
+++ Sources/UI/Views/EmailLoginView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct EmailLoginView: View {
+    @State var dataModel: EmailLoginDataModel
+    @FocusState var selectedField: LoginSelectedField?
+    
+    private func tryLogin() async throws {
+        selectedField = nil
+        try await dataModel.tryLogin()
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if let errorMessage = dataModel.loginErrorMessage {
+                Text(errorMessage)
+                    .bold()
+                    .foregroundColor(.red)
+            }
+        }
     }
 }
 
+
+// MARK: - Preview
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        EmailLoginView()
+        EmailLoginView(dataModel: dataModel)
+    }
+    
+    static var dataModel: EmailLoginDataModel {
+        EmailLoginDataModel(showResetPassword: { }, emailLogin: { _, _ in })
     }
 }
