9a811005a80d792c111ce1ec189f55d8f23479f8
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-05-28 17:22:46 -0700

    deleted old code

--- Sources/Composer/Composer.swift
+++ Sources/Composer/Composer.swift
@@ -7,24 +7,16 @@
 
 import SwiftUI
 
-public typealias EmailLoginInfo = (email: String, password: String)
-
 public func makeEmailLoginView(isEditingTextFields: Binding<Bool>? = nil, canShowResetPassword: Bool, emailLogin: @escaping (String, String) async throws -> Void) -> some View {
     let dataModel = EmailLoginDataModel(canShowResetPassword: canShowResetPassword, emailLogin: emailLogin)
 
     return EmailLoginView(isEditingTextFields: isEditingTextFields, dataModel: dataModel)
 }
 
-public func makeLoginView(colorOptions: LoginColorOptions = LoginColorOptions(), guestLogin: (() async throws -> Void)? = nil, emailLogin: ((EmailLoginInfo) async throws -> Void)? = nil, emailSignUp: (@escaping (EmailLoginInfo) async throws -> Void), resetPassword: ((String) async throws -> Void)? = nil) -> some View {
-    
-    let dataModel = LoginDataModel(colorOptions: colorOptions, guestLogin: guestLogin, emailLogin: emailLogin, emailSignUp: emailSignUp)
-    let resetView = makeResetPasswordView(colorOptions: colorOptions, resetPassword: resetPassword ?? emptyMethod)
-    
-    return LoginView(dataModel: dataModel, resetView: resetView, canShowResetView: resetPassword != nil)
+public func makeAppleSignInButton(appleSignIn: @escaping (AppleTokenInfo) throws -> Void) -> some View {
+    return CustomAppleButton(appleSignIn: appleSignIn)
 }
 
-/// used as a filler method when resetPassword is nil
-private func emptyMethod(_ email: String) async throws { }
-private func makeResetPasswordView(colorOptions: LoginColorOptions, resetPassword: (@escaping (String) async throws -> Void)) -> some View {
-    return ResetPasswordView(dataModel: ResetPasswordDataModel(colorOptions: colorOptions, sendResetPassswordEmail: resetPassword))
+public func makeGoogleSignInButton(googleSignIn: @escaping (GoogleTokenInfo?) async throws -> Void) -> some View {
+    return CustomGoogleButton(googleSignIn: googleSignIn)
 }
