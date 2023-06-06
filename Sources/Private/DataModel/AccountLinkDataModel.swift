67c9a0442bc500fc7bd9507d52466ba141a2864a
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-06-05 17:43:32 -0700

    WIP -> moved AccountLinkDataModel to its own file

---
+++ Sources/Private/DataModel/AccountLinkDataModel.swift
@@ -0,0 +1,57 @@
+//
+//  AccountLinkDataModel.swift
+//  
+//
+//  Created by Nikolai Nobadi on 6/5/23.
+//
+
+import Foundation
+
+final class AccountLinkDataModel: ObservableObject {
+    @Published var showingEmailView = false
+}
+
+
+// MARK: - ViewModel
+extension AccountLinkDataModel {
+    var passwordEmail: String? { "tester@gmail.com" }
+    var appleEmail: String? { nil }
+    var googleEmail: String? { nil }
+    
+    var accountLinkTypes: [AccountLinkType] {
+        return [.email(passwordEmail), .apple(appleEmail), .google(googleEmail)]
+    }
+    
+    func performLinkAction(for linkType: AccountLinkType) async throws {
+        guard linkType.email == nil else { return try await unlinkAccount() }
+        
+        switch linkType {
+        case .email:
+            try await showEmailLogin()
+        case .apple:
+            try await appleAccountLink()
+        case .google:
+            try await googleAccountLink()
+        }
+    }
+}
+
+
+// MARK: - Private Methods
+private extension AccountLinkDataModel {
+    @MainActor func showEmailLogin() async throws {
+        showingEmailView = true
+    }
+    
+    func appleAccountLink() async throws {
+        
+    }
+    
+    func googleAccountLink() async throws {
+        
+    }
+    
+    func unlinkAccount() async throws {
+        
+    }
+}
