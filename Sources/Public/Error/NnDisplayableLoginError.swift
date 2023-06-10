75bb907577f26dcc5e4db9a3844b83596c827968
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-06-10 16:26:04 -0700

    added documentation for NnDisplayableLoginError

--- Sources/Public/Error/NnDisplayableLoginError.swift
+++ Sources/Public/Error/NnDisplayableLoginError.swift
@@ -7,7 +7,17 @@
 
 import Foundation
 
-public protocol NnDisplayableLoginError {
+/// `NnDisplayableLoginError` is a public protocol that represents errors thrown during the login process.
+/// Errors conforming to this protocol can provide a title and a message that will be displayed to the user.
+///
+/// This protocol is particularly useful for providing user-friendly error information for non-validation
+/// errors (e.g., network errors, service errors, etc.). Email and password validation errors are handled
+/// internally by NnLoginKit and displayed directly in the login view.
+public protocol NnDisplayableLoginError: Error {
+    /// The title of the error. This is typically a short string that summarizes the error.
     var title: String { get }
+    
+    /// The message of the error. This is typically a longer string that describes the error in more detail.
     var message: String { get }
 }
+
