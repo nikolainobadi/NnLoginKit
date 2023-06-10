0ac696e6265f568acdfbee085816e1b01efac2c3
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-06-10 16:04:36 -0700

    added documentation for NnAccountLinkAuth

--- Sources/Public/Authentication/NnAccountLinkAuth.swift
+++ Sources/Public/Authentication/NnAccountLinkAuth.swift
@@ -7,7 +7,23 @@
 
 import Foundation
 
+/// `NnAccountLinkAuth` is a public protocol that defines methods related to linking and unlinking accounts.
+/// It is designed to work with Firebase Authentication and uses provider IDs for account linking/unlinking.
+/// The provider IDs are as follows:
+/// - For Email/Password: "password"
+/// - For Google: "google.com"
+/// - For Apple: "apple.com"
 public protocol NnAccountLinkAuth {
+    /// Asynchronously unlinks an account from the specified provider.
+    ///
+    /// - Parameter fromProvider: The provider from which the account should be unlinked. This should be
+    ///   one of the provider IDs mentioned above.
+    ///
+    /// - Throws: An error if the unlinking process fails.
     func unlink(fromProvider: String) async throws
+    
+    /// Loads the set of available account link types.
+    ///
+    /// - Returns: A `Set` of `AccountLinkType` objects representing the available types of accounts that can be linked.
     func loadAvailableAccountLinkTypes() -> Set<AccountLinkType>
 }
