6f5d81b9dd2526db39d6b6649dc03e06b9b797b6
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-06-10 16:41:38 -0700

    added documentation for makeAccountLinkView

--- Sources/Public/Composer/Composer.swift
+++ Sources/Public/Composer/Composer.swift
@@ -23,9 +23,20 @@ public func makeLoginView(titleImage: Image? = nil, textConfig: NnLoginTextConfi
 }
 
 
+/// Creates an instance of `AccountLinkView` with the provided configuration options.
+///
+/// - Parameters:
+///   - sectionTitle: A `String` representing the title of the section displaying the sign-in methods. Defaults to `"Sign-in Methods"`.
+///   - linkButtonTint: A `Color` representing the tint color of the Link/Unlink button. Defaults to `.blue`.
+///   - auth: An `NnAccountLinkAuth` instance for handling the linking and unlinking process.
+///   - isLoading: A `Binding<Bool>` instance indicating whether the view is currently loading.
+///   - setAuthenticationStatus: An optional closure invoked when the authentication status changes. It accepts a `Bool` parameter indicating the new authentication status. Defaults to `nil`.
+///
+/// - Returns: A `View` instance configured as an account link screen with the provided options.
 public func makeAccountLinkView(sectionTitle: String = "Sign-in Methods", linkButtonTint: Color = .blue, auth: NnAccountLinkAuth, isLoading: Binding<Bool>, setAuthenticationStatus: ((Bool) -> Void)? = nil) -> some View {
     
     let dataModel = AccountLinkDataModel(auth: auth, setAuthenticationStatus: setAuthenticationStatus)
     
     return AccountLinkView(isLoading: isLoading, dataModel: dataModel, sectionTitle: sectionTitle, linkButtonTint: linkButtonTint)
 }
+
