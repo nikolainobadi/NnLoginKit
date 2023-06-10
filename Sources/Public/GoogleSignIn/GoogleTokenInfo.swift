22a4c27655bc6e931784a4d6a09e4bfb73b9b4c8
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-06-10 16:06:17 -0700

    modified documentation for AppleTokenInfo and GoogleTokenInfo

--- Sources/Public/GoogleSignIn/GoogleTokenInfo.swift
+++ Sources/Public/GoogleSignIn/GoogleTokenInfo.swift
@@ -6,8 +6,9 @@
 //
 
 /// `GoogleTokenInfo` is a typealias for a tuple containing information related to a Google ID token and access token.
+/// This structure is designed for use with Firebase Authentication.
 ///
-/// The `idTokenString` is a string representation of the ID token that is provided by Google after a successful authentication.
+/// The `idTokenString` is a string representation of the ID token that is provided by Google after a successful authentication and is used for Firebase authentication.
 /// The `accessTokenString` is a string representation of the access token that can be used to authenticate API calls to Google services.
 public typealias GoogleTokenInfo = (idTokenString: String, accessTokenString: String)
 
