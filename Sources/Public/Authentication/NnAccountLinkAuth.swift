8d033bf1848a0e70301d6aa5a41c7825cf22f785
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2023-06-13 16:47:21 -0700

    added more clarifying code snippets to documentation for <NnAccountLinkAuth>

--- Sources/Public/Authentication/NnAccountLinkAuth.swift
+++ Sources/Public/Authentication/NnAccountLinkAuth.swift
@@ -14,6 +14,14 @@ import Foundation
 /// - For Google: "google.com"
 /// - For Apple: "apple.com"
 ///
+/// The `loadAvailableAccountLinkTypes()` method should return a set of `NnAccountLinkType` instances
+/// representing the types of accounts the developer would like to display in the `AccountLinkView`.
+/// For example, if the developer only wants to allow Apple and Google sign-in methods,
+/// the method should only return the `.apple` and `.google` cases of `NnAccountLinkType`.
+///
+/// It's important to note that the `loadAvailableAccountLinkTypes()` should also provide the
+/// appropriate associated values (i.e., email and action) with each `NnAccountLinkType` case.
+///
 /// ## Example usage:
 /// Here is how you might implement a class that conforms to `NnAccountLinkAuth`:
 ///
@@ -23,8 +31,27 @@ import Foundation
 ///         // code to unlink from the specified provider
 ///     }
 ///
-///     func loadAvailableAccountLinkTypes() -> Set<AccountLinkType> {
-///         // code to load the available account link types
+///     func loadAvailableAccountLinkTypes() -> Set<NnAccountLinkType> {
+///         // only return .apple and .google to allow only these sign-in methods
+///
+///         let appleLinkAction: NnAccountLinkType.AppleLinkAction = { tokenInfo in
+///             // code to link with Apple
+///         }
+///
+///         let googleLinkAction: NnAccountLinkType.GoogleLinkAction = { tokenInfo in
+///             // code to link with Google
+///         }
+///
+///         let currentUser = Auth.auth().currentUser // requires FirebaseAuth
+///
+///         let googleEmail = currentUser?.providerData.first(where: { $0.providerID == "google.com" })?.email
+///
+///         let appleEmail =  currentUser?.providerData.first(where: { $0.providerID == "apple.com" })?.email
+///
+///         return [
+///             .apple(appleEmail, appleLinkAction),
+///             .google(googleEmail, googleLinkAction)
+///         ]
 ///     }
 /// }
 ///
@@ -43,6 +70,6 @@ public protocol NnAccountLinkAuth {
     
     /// Loads the set of available account link types.
     ///
-    /// - Returns: A `Set` of `AccountLinkType` objects representing the available types of accounts that can be linked.
+    /// - Returns: A `Set` of `NnAccountLinkType` objects representing the types of accounts that can be linked.
     func loadAvailableAccountLinkTypes() -> Set<NnAccountLinkType>
 }
